57d10df92ce69dda23a1de58cba03086cd5f6a33
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-31 21:32:43 +0200

    Move code into CardDisplay

--- src/App.jsx
+++ src/App.jsx
@@ -39,25 +39,7 @@ function App() {
 
   return (
     <>
-      <CardDisplay />
-      <div className="pokemon-container">
-        {pokemon.map((monster) => {
-          return (
-            <div key={monster.id} className="card">
-              <div>
-                <img
-                  src={
-                    monster.sprites.versions['generation-i']['red-blue']
-                      .front_default
-                  }
-                  alt={monster.name}
-                />
-              </div>
-              <div>{monster.name}</div>
-            </div>
-          );
-        })}
-      </div>
+      <CardDisplay pokemon={pokemon} />
     </>
   );
 }
--- src/components/CardDisplay.jsx
+++ src/components/CardDisplay.jsx
@@ -1,7 +1,24 @@
-export default function CardDisplay() {
+export default function CardDisplay({ pokemon }) {
   return (
     <>
-      <div>CardDisplay</div>
+      <div className="pokemon-container">
+        {pokemon.map((monster) => {
+          return (
+            <div key={monster.id} className="card">
+              <div>
+                <img
+                  src={
+                    monster.sprites.versions['generation-i']['red-blue']
+                      .front_default
+                  }
+                  alt={monster.name}
+                />
+              </div>
+              <div>{monster.name}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
