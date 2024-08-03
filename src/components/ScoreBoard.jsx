d88b660f0dc3e1c9eff65f796e087f77dac17deb
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-03 16:36:09 +0200

    Display current score and best score

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import '../styles/scoreboard.css';
 
 export default function ScoreBoard() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <>
       <div id="score-board">
         <div>
-          <h3>Current Score:</h3>
+          <h3>Current Score: {currentScore}</h3>
         </div>
         <div>
-          <h3>Best Score:</h3>
+          <h3>Best Score: {bestScore}</h3>
         </div>
       </div>
     </>
